6b1d024e420642464e1541f893fda6c8d80f5fc8
daw2 <daw2@192.168.56.1>
2023-12-12 10:51:23 +0100

    Version_1.2 estable

--- scriptDB/CreaDB208DWESProyectoTema5Explotacion.php
+++ scriptDB/CreaDB208DWESProyectoTema5Explotacion.php
@@ -51,8 +51,8 @@
     T01_CodUsuario CHAR(8) NOT NULL CHECK(CHAR_LENGTH(T01_CodUsuario) BETWEEN 4 AND 8),
     T01_Password VARCHAR(255) NOT NULL,
     T01_DescUsuario VARCHAR(255) NOT NULL,
-    T01_NumConexiones INT DEFAULT 1 NOT NULL DEFAULT 1,
-    T01_FechaHoraUltimaConexion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    T01_NumConexiones INT NOT NULL DEFAULT 0,
+    T01_FechaHoraUltimaConexion DATETIME,
     T01_Perfil ENUM('usuario', 'administrador') NOT NULL DEFAULT 'usuario',
     T01_ImagenUsuario LONGBLOB,
     PRIMARY KEY (T01_CodUsuario));";
